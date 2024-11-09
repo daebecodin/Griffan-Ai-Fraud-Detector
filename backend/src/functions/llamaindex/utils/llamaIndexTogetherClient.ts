6086cc92e11b06f438aa3ba32da4196eaaee007c
daebecodin <136534615+daebecodin@users.noreply.github.com>
2024-11-09 11:56:00 -0800

    init commit
    init commit

---
+++ backend/src/functions/llamaindex/utils/llamaIndexTogetherClient.ts
@@ -0,0 +1,10 @@
+import { TogetherLLM, Settings } from "llamaindex";
+
+export function llamaIndexTogetherClient({ model }: { model: TogetherLLM["model"] }) {
+    Settings.llm = new TogetherLLM({
+        apiKey: process.env.TOGETHER_API_KEY,
+        model: model,
+    });
+    return Settings.llm;
+}
+
